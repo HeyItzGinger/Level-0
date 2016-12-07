2922bc21c35561eab9acbdeecc404fc9a4ce555b
Ryguy-1 <league@WTS8.attlocal.net>
2016-12-06 16:55:30 -0800

    Cool Stuffs

--- src/ObedientTortoise.java
+++ src/ObedientTortoise.java
@@ -2,15 +2,38 @@ import org.teachingextensions.logo.Tortoise;
 
 public class ObedientTortoise {
 	public static void main(String[] args) {
+		Tortoise.setSpeed(10);
 		Tortoise.show();
 		Tortoise.penDown();
 		drawSquare();
+		drawTriangle();
+		drawCircle();
+		drawShape(1);
 	}
 
 	static void drawSquare() {
 		for (int square = 0; square < 4; square++) {
 			Tortoise.move(50);
-			Tortoise.turn(90);
+			Tortoise.turn(360/4);
+		}
+	}
+	static void drawTriangle() {
+		for(int triangle = 0; triangle <3; triangle++){
+			Tortoise.move(50);
+			Tortoise.turn(360/3);
+			
+		}
+	}
+	static void drawCircle() {
+		for(int circle = 0; circle<360; circle++){
+			Tortoise.move(1);
+			Tortoise.turn(360/360);
+		}
+	}
+	static void drawShape(int numSides) {
+		for(int shape = 0; shape < numSides; shape++){
+			Tortoise.move(2);
+		Tortoise.turn(360/numSides);
 		}
 	}
 }
---
+++ src/Validation.java
@@ -0,0 +1,43 @@
+
+import java.util.Random;
+
+
+import javax.swing.JOptionPane;
+
+
+public class Validation {
+	public static void main(String[] args) {
+
+
+		// 1. Watch the first 2 minutes of this movie: http://vimeo.com/2485018
+		int randomNumber = new Random().nextInt(5);
+		// 2. On paper, write all the numbers that get printed when you run this class
+		System.out.println(randomNumber);
+
+
+		// 3. Use the randomNumber to give the user a random compliment.
+	if(randomNumber == 0){
+		JOptionPane.showMessageDialog(null, "You're eyes are Beautiful!");
+	}
+	else if(randomNumber == 1){
+		JOptionPane.showMessageDialog(null, "You're Awesome!");
+	}
+	else if (randomNumber == 2){
+		JOptionPane.showMessageDialog(null, "Nice clothes!");
+	}
+	else if (randomNumber == 3){
+		JOptionPane.showMessageDialog(null, "You're really nice!");
+	}
+	else if (randomNumber == 4){
+		JOptionPane.showMessageDialog(null, "Like your Hair!");
+	}
+
+		// 4. Repeat all the code above 10 times
+		
+		// 5. Find someone to test out your program. They will like it :)
+	}
+}
+
+
+
+
